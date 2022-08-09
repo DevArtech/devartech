ddd547ce76a8bac7d8c32a5c717b82f6a1e5ae5a
DevArtech <100626221+DevArtech@users.noreply.github.com>
2022-08-09 14:16:43 -0500

    Add header hide code

---
+++ code.js
@@ -0,0 +1,52 @@
+(function(){
+
+    var doc = document.documentElement;
+    var w   = window;
+
+    var curScroll;
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curDirection = 0;
+    var prevDirection = 0;
+
+    var header = document.getElementById('site-header');
+    var toggled;
+    var threshold = 200;
+
+    var checkScroll = function() {
+        curScroll = w.scrollY || doc.scrollTop;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+    };
+
+    var toggleHeader = function() { 
+        toggled = true;
+        if(curDirection === 2 && curScroll > threshold) {
+            header.classList.add('hide');
+        }
+        else if (curDirection === 1) {
+            header.classList.remove('hide');
+        }
+        else {
+            toggled = false;
+        }
+        return toggled;
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+})();
